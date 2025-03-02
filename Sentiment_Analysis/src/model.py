ab5773d3d4c09cff399bfc8c567ad48ea90edd31
giuli-c <91190979+giuli-c@users.noreply.github.com>
2025-03-02 15:15:35 +0000

    prova

--- Sentiment_Analysis/src/model.py
+++ Sentiment_Analysis/src/model.py
@@ -44,3 +44,4 @@ def test_model(text, sentiment_classes=LIST_SENTIMENT):
 if __name__ == "__main__":
     text = "Covid cases are increasing fast!"
     print(f"Testo: {text} → Sentiment: {test_model(text)}")
+    
